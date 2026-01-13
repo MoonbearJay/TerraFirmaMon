51d242b2494023cbdaf0bbbb74ed9c9d4fa176f7
ChocoboJay <DuffyJ1010@gmail.com>
2026-01-12 20:45:18 -0800

    0.4.0 "Everything Everywhere"

---
+++ overrides/kubejs/server_scripts/interaction_whitelist.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('block', event => {
+  event.add('ftbchunks:interact_whitelist', '#waystones:waystones'),
+  event.add('ftbchunks:interact_whitelist', 'cobblemon:pc'),
+  event.add('ftbchunks:interact_whitelist', 'cobblemon:healing_machine')
+})
---
+++ overrides/kubejs/server_scripts/tumblestone_heat_source_block.js
@@ -0,0 +1,3 @@
+ServerEvents.tags('block', event => {
+    event.add('cobblemon:tumblestone_heat_source', 'tfc:rock/magma/basalt')
+})
